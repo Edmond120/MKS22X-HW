55da7ed58652ab267a52029a73f3e2b06b44f953
edmond120 <edmondwong888@msn.com>
2017-02-17 19:10:43 -0500

    did fastSort's init

--- 03KnightsTour/KnightBoard.java
+++ 03KnightsTour/KnightBoard.java
@@ -1,8 +1,12 @@
 public class KnightBoard{
     private int[][]board;
+    private int xLength;
+    private int yLength;
+    private int stepNum = 0;
     boolean show;
     public KnightBoard(int startingRows,int startingCols) {
-	board = new int[startingRows,startingCols];
+	xLength = startingRows;
+	yLength = startingCols;
     }
 
     public String toString(){
@@ -22,5 +26,90 @@ public class KnightBoard{
     }
 	
     private boolean solveH(int row ,int col, int number){
+    }
+    private void solve21x21(){
+    }
+    private boolean _3Or7;
+    private int hallowX;
+    private int hallowY;
+    private int thicknessW;
+    private int thicknessL;
+    private int width;
+    private int length;
+    private void solveSquareH(){
+    }
+    private void solveSquare(){
+	if(xLength % 21 == 0){
+	    solve21x21();
+	}
+	else{
+	    int halve = xLength / 2;
+	    width = (halve / 3) * 3;
+	    length = (halve / 7) * 7;
+	    int remainder = xLength - (width + height);
+	    //just going to hard code this part since there is only 9 possibilities
+	    if(remainder == 1){
+		width -= 6;
+		length += 7;
+	    }
+	    else if(remainder == 2){
+		width += 9;
+		length -= 7;
+	    }
+	    else if(remainder == 3){
+		width += 3;
+	    }
+	    else if(remainder == 4){
+		width -= 3;
+		length += 7;
+	    }
+	    else if(remainder == 5){
+		width -= 9;
+		length += 14;
+	    }
+	    else if(remainder == 6){
+		width += 6;
+	    }
+	    else if(remainder == 7){
+		length += 7;
+	    }
+	    else if(remainder == 8){
+		width -= 6;
+		length += 14;
+	    }
+	    else if(remainder == 9){
+		width += 9;
+	    }
+	    if(width > length){
+		_3Or7 = true;
+	    }
+	    else{
+		_3or7 = false;
+	    }
+	    if(_30r7){
+		hallowX = width - length;
+	    }
+	    else{
+		hallowX = length - width;
+	    }
+	    thicknessW = width / 3;
+	    thicknessL = length / 7;
+	    solveSquareH();
+	}
+    }
+    public void fastSolve(){
+	if(xLength == yLength){
+	    if(xLength > 20){
+		solveSquare();
+		return;
+	    }
+	}
+	else if(xLength >= 3 && yLength >= 3 && (xLength >= 21 || yLength >= 21)){
+	    solveRectangle();
+	    return;
+	}
+	else{
+	    System.out.println("fastSolve is only for boards that meet these requirements \n 1. Both sides must be >= 3 \n 2. One of those sides is >= 21");
+	}
     }
 }
