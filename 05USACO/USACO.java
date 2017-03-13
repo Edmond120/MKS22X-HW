c565dfd0298b3ae5a1fc3b22ba9da010bd30fba2
edmond120 <edmondwong888@msn.com>
2017-03-12 22:47:44 -0400

    bugs in silver

--- 05USACO/USACO.java
+++ 05USACO/USACO.java
@@ -56,9 +56,42 @@ public class USACO{
 	}
 	catch(Throwable e){
 	    e.printStackTrace();
+	    /*
+	    System.out.println("error");
+	    System.out.println(index);
+	    System.out.println(temp);
+	    System.out.println(end);
+	    System.out.println(s.s);
+	    System.out.println(ary[0] + " " + ary[1] + " " + ary[2]);
+	    */
 	    return false;
 	}
 	s.s = s.s.substring(end + 1);
+	return true;
+    }
+    private boolean lineToArray(char[]ary,oString s){
+	int index = 0;
+	try{
+	    for(int i = 0; i < s.s.length() && index < ary.length; i++){
+		if(s.s.charAt(i) == '\n'){
+		    s.s = s.s.substring(i + 1);
+		    break;
+		}
+		else{
+		    ary[index++] = s.s.charAt(i);
+		}
+		//System.out.println(end);
+	    }
+	}
+	catch(Throwable e){
+	    //System.out.println("sup");
+	    e.printStackTrace();
+	    //System.out.println("sup");
+	    return false;
+	}
+	
+	s.s = s.s.substring(ary.length);
+	//System.out.println(s.s);
 	return true;
     }
     private String firstLine(String x){
@@ -71,9 +104,76 @@ public class USACO{
     }
 
     public int silver(String fileName){
-	return 0;
+	oString file = new oString(getFile(fileName));
+	int[]parms = new int[3];
+	if(!lineToArray(parms,file)){
+	    System.out.println("fileCorrupt1");
+	    return 0;
+	}
+	char[][]board = new char[parms[0]][parms[1]];
+	for(int r = 0; r < parms[0]; r++){
+	    if(!lineToArray(board[r],file)){
+		System.out.println("fileCorrupt2");
+		return 0;
+	    }
+	}
+	int[]cords = new int[4];
+	file.s = file.s.substring(1);
+	if(!lineToArray(cords,file)){
+	    System.out.println("fileCorrupt3");
+	    return 0;
+	}
+	int[][]board1 = new int[parms[0]][parms[1]];
+	for(int r = 0; r < parms[0]; r++){
+	    for(int c = 0; c < parms[1]; c++){
+		if(board[r][c] == '*'){
+		    board1[r][c] = -1;
+		}
+	    }
+	}
+	board1[cords[0] - 1][cords[1] - 1] = 1;
+	int[][]board2 = new int[parms[0]][parms[1]];
+	for(int r = 0; r < board2.length; r++){
+	    for(int c = 0; c < board2[r].length; c++){
+		board2[r][c] = board1[r][c];
+	    }
+	}
+	int[][] boardT;
+	for(int a = 0; a < parms[2] - 1;a++){
+	    for(int r = 0; r < board1.length;r++){
+		for(int c = 0; c < board1[r].length;c++){
+		    if(board1[r][c] != -1){
+			for(int n = 0; n < 8; n += 2){
+			    if(r + dir[n] >= 0 && c + dir[n + 1] >= 0 &&
+			       r + dir[n] < parms[0] && c + dir[n + 1] < parms[1] &&
+			       board2[r + dir[n]][c + dir[n + 1]] != -1){
+				board2[r + dir[n]][c + dir[n + 1]] += board1[r][c];
+			    }
+			}
+			if(board1[r][c] != -1){
+			    board1[r][c] = 0;
+			}
+		    }
+		}
+	    }
+	    //show(board1);
+	    //show(board2);
+	    boardT = board1;
+	    board1 = board2;
+	    board2 = boardT;
+	}
+	return board1[parms[0] - 1][parms[1] - 1];
     }
-
+    private static void show(int[][]b){
+	for(int r = 0; r < b.length;r++){
+	    for(int c = 0; c < b[r].length;c++){
+		System.out.print(b[r][c] + " ");
+	    }
+	    System.out.print('\n');
+	}
+	System.out.print('\n');
+    }
+    private static int[]dir = {0,1,0,-1,1,0,-1,0};
     public String getFile(String fileName){
 	String file = "";
 	try{
