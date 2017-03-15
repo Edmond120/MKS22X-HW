030290798da26979a37d3f75a3a4c5b2abd45ccc
edmond120 <edmondwong888@msn.com>
2017-03-15 18:55:59 -0400

    done

--- 07Quick/Quick.java
+++ 07Quick/Quick.java
@@ -1,8 +1,8 @@
 import java.util.Random;
 public class Quick{
     public static int part ( int [] data, int start, int end){
-	int r = new Random().nextInt(data.length);
-	System.out.println(data[r]);
+	int r = new Random().nextInt(end - start + 1) + start;
+	//System.out.println(data[r]);
 	int t = data[r];
 	data[r] = data[start];
 	r = start;
@@ -35,54 +35,60 @@ public class Quick{
 	    t = data[start];
 	    data[start] = data[r];
 	    data[r] = t;
+	    //System.out.println("a");
 	}
 	else{
 	    if(start > 1){
 		t = data[start - 1];
-	        data[start - 1] = data[r];
+	        data[start-- - 1] = data[r];
 		data[r] = t;
+		//System.out.println("b");
 	    }
-	    else{start--;};
+	    else{start--;
+		//System.out.println("c");
+	    }
 	}
 	return start;
 	
 	
     }
-    /*
-    public static int[] partition(int[]ary,int start, int end,boolean randomNum,int num){//end is inclusive
-	int[]temp = new int[end - start + 1];
-	for(int i = start; i <= end; i++){
-	    temp[i] = ary[i];
+    public static int quickselect(int []data, int k){
+	int start = 0;
+	int end = data.length - 1;
+	int i = part(data,start,end);
+	if(k >= data.length){
+	    System.out.println("k is bigger than the array");
+	    return 0;
 	}
-	if(randomNum){
-	    int x = new Random().nextInt(temp.length);
-	    if(x != 0){
-		int t = temp[x];
-		temp[x] = temp[0];
-		temp[0] = t;
+	if(k == data.length - 1){
+	    int aaa = data[0];
+	    for(int iii = 1; iii < data.length; iii++){
+		if(data[iii] > aaa){
+		    aaa = data[iii];
+		}
 	    }
+	    return aaa;
 	}
-	else{
-	    int t = temp[num];
-	    temp[num] = temp[0];
-	    temp[0] = t;
-	}
-	for(int i = 1; i < temp.length;i++){
-	    if(temp[i] < temp[0]){
-		ary[start++] = temp[i];
+		
+	while(i != k){
+	    if(i > k){
+		end = i;
 	    }
-	    else{
-		ary[end--] = temp[i];
+	    else if(i < k){
+		start = i;
 	    }
+	    i = part(data,start,end);
 	}
-	ary[start] = temp[0];
-	return ary;
+	return data[i];
     }
-    */
+  //return the value that is the kth smallest value of the array. 
+  //use your partition method to help you accomplish this.
     public static void main(String[]ary){
+	int n = Integer.parseInt(ary[0]);
         int[]x = {0,4,2,7,3,5,7,1};
 	//partition(x,0,7,false,4);
-	System.out.println(part(x,0,7));
+	//System.out.println(part(x,0,7));
+	System.out.println(quickselect(x,n));
 	for(int i = 0; i < x.length; i++){
 	    System.out.print(x[i] + " ");
 	}
