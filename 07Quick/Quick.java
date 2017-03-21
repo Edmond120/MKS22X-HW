1c301c254f6895eb0b7f207abcd150ff79e83f4b
edmond120 <edmondwong888@msn.com>
2017-03-21 15:34:12 -0400

    bug

--- 07Quick/Quick.java
+++ 07Quick/Quick.java
@@ -1,6 +1,28 @@
 import java.util.Random;
 public class Quick{
-    public static int part ( int [] data, int start, int end){
+    private static void swap(int[]ary,int a,int b){
+	int temp = ary[a];
+	ary[a] = ary[b];
+	ary[b] = temp;
+    }
+    public static int part(int[]ary,int start,int end){
+	int r = new Random().nextInt(end - start + 1) + start;
+	int v = ary[r];
+	int i = start;
+	while(i != end){
+	    if(ary[i] == v){
+		i++;
+	    }
+	    else if(ary[i] < v){
+		swap(ary,i++,start++);
+	    }
+	    else{
+		swap(ary,i,end--);
+	    }
+	}
+	return i;
+    }
+    private static int partOld ( int [] data, int start, int end){
 	int r = new Random().nextInt(end - start + 1) + start;
 	//System.out.println(data[r]);
 	int t = data[r];
@@ -81,14 +103,37 @@ public class Quick{
 	}
 	return data[i];
     }
+    public static int[] quickSort(int[]ary){
+	int p = part(ary,0,ary.length - 1);
+	quickSortH(ary,0,p);
+	quickSortH(ary,p,ary.length - 1);
+	return ary;
+    }
+    private static void quickSortH(int[]ary,int start,int end){
+	if(end - start <= 1){
+	    return;
+	}
+	else{
+	    int p = part(ary,start,end);
+	    quickSortH(ary,start,p);
+	    quickSortH(ary,p,end);
+	}
+    }
   //return the value that is the kth smallest value of the array. 
   //use your partition method to help you accomplish this.
     public static void main(String[]ary){
 	int n = Integer.parseInt(ary[0]);
-        int[]x = {0,4,2,7,3,5,7,1};
-	//partition(x,0,7,false,4);
-	//System.out.println(part(x,0,7));
-	System.out.println(quickselect(x,n));
+	int[]x = new int[n];
+	Random r = new Random();
+	for(int a = 0; a < x.length; a++){
+	    x[a] = r.nextInt(100);
+	}
+	for(int i = 0; i < x.length; i++){
+	    System.out.print(x[i] + " ");
+	}
+	System.out.print("\n");
+	quickSort(x);
+	System.out.println("sorted");
 	for(int i = 0; i < x.length; i++){
 	    System.out.print(x[i] + " ");
 	}
