5a770409e9d60faa1cebbfccab388a069aee24e1
edmond120 <edmondwong888@msn.com>
2017-03-24 12:09:57 -0400

    a

--- 08MergeSort/Merge.java
+++ 08MergeSort/Merge.java
@@ -9,9 +9,20 @@ class Timer{
 	return (System.currentTimeMillis() - time) / 1000;
     }
 }
+// [0,1,2,3,4,5,6,7,8,9,10,11]
 public class Merge{
     public static void mergesort(int[]ary){
+	int sets = ary.length;
+	int setSize = 1;
+	int selectedSet = 1;
+	int start;
+	int end;
+	while(selectedSet <= sets){
+	    sets = (sets / 2) + (sets % 2);
+	    setSize = setSize * 2;
+	    
     }
+    
     public static String toString(int[]ary){
 	String s = "";
 	for(int i = 0; i < ary.length; i++){
@@ -21,5 +32,7 @@ public class Merge{
     }
     public static void print(int[]ary){
 	System.out.println(toString(ary));
+    }
+    public static void main(String[]args){
     }
 }
