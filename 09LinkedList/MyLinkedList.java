8697bd7832027cfd421adea8f48b5ff5828b465e
edmond120 <edmondwong888@msn.com>
2017-03-29 22:19:24 -0400

    linked list almost done

--- 09LinkedList/MyLinkedList.java
+++ 09LinkedList/MyLinkedList.java
@@ -8,58 +8,110 @@ class Timer{
     public long stop(){
 	return (System.currentTimeMillis() - time) / 1000;
     }
+    public static String toString(int[]ary){
+	String result = "";
+	for(int i = 0; i < ary.length;i++){
+	    result += ary[i] + " ";
+	}
+	return result;
+    }
 }
 public class MyLinkedList{
-    Lnode start = null;
-    Lnode end = null;
-    Lnode current = null;
-    int currentPlace = 0;
+    Lnode start;
+    Lnode end;
+    Lnode current;
+    int currentPlace = -1;
     int size = 0;
+    public int[] toArray(){
+	int[]ary = new int[size];
+	for(int i = 0; i < size;i++){
+	    ary[i] = get(i);
+	}
+	return ary;
+    }
     public MyLinkedList(){
+	start = new Lnode();
+	start.after = new Lnode();
+	start.after.before = start;
+	end = start;
+	current = start;
     }
     public MyLinkedList(int[]array){
 	
     }
-    public add(int num){
-	add(num,0);
+    public int get(int index){
+	return select(index).num;
     }
-    public add(int num,int i){
-	int a = currentPlace - i;
-	if(a < 0){
-	    a *= -1;
+    public boolean add(int value){
+	addH(value,end);
+	end = end.after;
+	return true;
+    }
+    public void addLast(int value){
+	add(value);
+    }
+    public void addStart(int value){
+	addH(value,start);
+    }
+    public void add(int value,int index){
+	addH(value,select(index));
+	if(index == size - 1){
+	    end = end.after;
 	}
-	if(a < i && a < size){
-	    go(i);
-	    addH(num);
+    }
+    private boolean goTo(int index){
+	if(index >= size){
+	    return false;
 	}
-	else if(a < size / 2){
-	    current = start;
-	    go(i);
-	    addH(num);
+	if(currentPlace > index){
+	    moveLeft(currentPlace - index);
 	}
 	else{
-	    current = end;
-	    go(i);
-	    addH(num);
+	    moveRight(index - currentPlace);
+	}
+	return true;
+	
+    }
+    private Lnode select(int index){
+	int a;
+	if(currentPlace > index){
+	    a = currentPlace - index;
+	}
+	else{
+	    a = index - currentPlace;
+	}
+	int b = (size - 1) - index;
+	if(index > b){
+	    if(b < a){
+		current = end;
+		currentPlace = size - 1;
+	    }	    	
+	}
+	else{
+	    if(index < a){
+		current = start.after;
+		currentPlace = 0;
+	    }
+	}
+	goTo(index);
+	return current;
+    }
+    private void addH(int value,Lnode node){
+	node.after.before = new Lnode(value,node,node.after);
+	node.after = node.after.before;
+	size++;
+    }
+    private void moveRight(int times){
+	while(times-- > 0){
+	    current = current.after;
+	    currentPlace++;
 	}
     }
-    private addH(num){
-	//-1,0,1,2,3,4,5
-	Lnode x = new Lnode(num,current.before,current);
-	current.before.after = x;
-	current.before = x;
-    }
-    private go(int i){
-    }
-    public static String toString(int[]ary){
-	String s = "[ ";
-	for(int i = 0; i < ary.length; i++){
-	    s += ary[i] + ", ";
+    private void moveLeft(int times){
+	while(times-- > 0){
+	    current = current.before;
+	    currentPlace--;
 	}
-	return s + "]";
-    }
-    public static void print(int[]ary){
-	System.out.println(toString(ary));
     }
     class Lnode{
 	Lnode before = null;
@@ -67,7 +119,7 @@ public class MyLinkedList{
 	int num;
 	Lnode(){
 	}
-	Lnode(int num,node before,node after){
+	Lnode(int num,Lnode before,Lnode after){
 	    this.num = num;
 	    this.before = before;
 	    this.after = after;
--- 09LinkedList/driver.java
+++ 09LinkedList/driver.java
@@ -1,4 +1,12 @@
 public class driver{
+    public static void print(MyLinkedList x){
+	System.out.println(Timer.toString(x.toArray()));
+    }
     public static void main(String[]args){
+	MyLinkedList x = new MyLinkedList();
+	for(int a = 1; a < 10; a++){
+	    x.add(a);
+	    print(x);
+	}
     }
 }
